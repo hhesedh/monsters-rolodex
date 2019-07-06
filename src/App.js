52c2da4d45c56ae88a91d154f87b261a6e18ae91
fabio <fabiodeoliveira07@gmail.com>
2019-07-06 15:37:02 -0300

    Finalização projeto

--- src/App.js
+++ src/App.js
@@ -13,6 +13,10 @@ class App extends Component {
     };
   }
 
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
+
   componentDidMount() {
     fetch('http://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
@@ -25,9 +29,10 @@ class App extends Component {
     );
     return (
       <div className="App">
+        <h1> Monsters Rolodex </h1>
         <SearchBox
           placeholder="search monsters"
-          handleChange={e => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
