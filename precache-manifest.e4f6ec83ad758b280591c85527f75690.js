45076b90d9e32bbf171dd68e47ce92a0b3aa5af9
fabio <fabiodeoliveira07@gmail.com>
2019-07-06 16:01:12 -0300

    Updates

---
+++ precache-manifest.e4f6ec83ad758b280591c85527f75690.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "273b040433bb7c460375cb29a02acd0f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a1695efda366f7d32c48",
+    "url": "/monsters-rolodex/static/css/main.a6ea5095.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a1695efda366f7d32c48",
+    "url": "/monsters-rolodex/static/js/main.5f124e68.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
